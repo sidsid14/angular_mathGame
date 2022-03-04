cf4e4a566092b136121f222b16048c6239b58e49
Sudhanshu Tiwari <sudhanshu@Sudhanshus-MacBook-Air.local>
2022-03-04 19:08:56 +0530

    Implementing statistic solution using Rxjs scan operator

--- src/app/equation/equation.component.ts
+++ src/app/equation/equation.component.ts
@@ -1,6 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import { AbstractControl, FormControl, FormGroup } from '@angular/forms';
-import { delay, filter } from 'rxjs';
+import { delay, filter, scan } from 'rxjs';
 import { MathValidators } from '../math-validators';
 
 @Component({
@@ -30,14 +30,21 @@ export class EquationComponent implements OnInit {
   constructor() {}
 
   ngOnInit(): void {
-    const startTime = new Date();
-    let numberSolved = 0;
-
     this.mathForm.statusChanges
       .pipe(filter((value) => value === 'VALID'))
       .pipe(delay(500))
-      .subscribe(() => {
-        numberSolved++;
+      .pipe(
+        scan(
+          (acc) => {
+            return {
+              numberSolved: acc.numberSolved + 1,
+              startTime: acc.startTime,
+            };
+          },
+          { numberSolved: 0, startTime: new Date() }
+        )
+      )
+      .subscribe(({ numberSolved, startTime }) => {
         this.secondsPerSolution =
           (new Date().getTime() - startTime.getTime()) / numberSolved / 1000;
 
